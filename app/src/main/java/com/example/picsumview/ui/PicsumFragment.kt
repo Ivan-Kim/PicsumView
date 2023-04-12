55aee3a1de4450d36aced10503170e8a5aba77a9
Ivan-Kim <57326300+Ivan-Kim@users.noreply.github.com>
2023-04-13 03:46:17 +0800

    그리드/리스트 디스플레이 옵션 기능 추가

--- app/src/main/java/com/example/picsumview/ui/PicsumFragment.kt
+++ app/src/main/java/com/example/picsumview/ui/PicsumFragment.kt
@@ -1,15 +1,16 @@
 package com.example.picsumview.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.picsumview.R
 import com.example.picsumview.databinding.FragmentPicsumBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -24,6 +25,8 @@ class PicsumFragment : Fragment() {
 
     private val viewModel: PicsumViewModel by viewModels()
 
+    private lateinit var picsumLayoutManager: GridLayoutManager
+
     private val picsumAdapter = PicsumAdapter()
 
     private var _binding: FragmentPicsumBinding? = null
@@ -34,8 +37,9 @@ class PicsumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPicsumBinding.inflate(layoutInflater)
+        picsumLayoutManager = GridLayoutManager(context, 2)
         binding.listPicsum.apply {
-            layoutManager = LinearLayoutManager(context)
+            layoutManager = picsumLayoutManager
             adapter = picsumAdapter
         }
         return binding.root
@@ -43,6 +47,7 @@ class PicsumFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupMenu()
         collectPictures()
     }
 
@@ -54,6 +59,32 @@ class PicsumFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) { }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.options_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.btn_switch_layout -> {
+                        if (picsumLayoutManager.spanCount == 1) {
+                            picsumLayoutManager.spanCount = 2
+                            menuItem.title = "Switch List View"
+                        } else {
+                            picsumLayoutManager.spanCount = 1
+                            menuItem.title = "Switch Grid View"
+                        }
+                        picsumAdapter.notifyDataSetChanged()
+                    }
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     override fun onDestroyView() {
